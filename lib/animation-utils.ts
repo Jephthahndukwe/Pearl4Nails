95ac911a22e8c8e986300199459f66a5b0ff37bb
Jephthah Ndukwe <69301693+Jephthahndukwe@users.noreply.github.com>
2025-05-12 13:33:28 +0100

    Update animation-utils.ts

--- lib/animation-utils.ts
+++ lib/animation-utils.ts
@@ -1,7 +1,7 @@
 export function hasAnimationExpired(): boolean {
   // Animation expiration date (1 week from launch)
-  const launchDate = new Date(2025, 4, 4) // May 4, 2025 (month is 0-based)
-  const expirationDate = new Date(launchDate.getTime() + 7 * 24 * 60 * 60 * 1000)
+  const launchDate = new Date(2025, 4, 12) // May 4, 2025 (month is 0-based)
+  const expirationDate = new Date(launchDate.getTime() + 12 * 24 * 60 * 60 * 1000)
   return new Date() > expirationDate
 }
 
